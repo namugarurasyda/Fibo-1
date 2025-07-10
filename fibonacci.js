b8f14360a9e7b00140c33fccaffbdbff22e5b52c
Conrad Waako K <68024054+elconrad23@users.noreply.github.com>
2025-07-10 10:58:15 +0300

    Update fibonacci.js

--- fibonacci.js
+++ fibonacci.js
@@ -1,17 +1,24 @@
-function fibonacci(n) {
+//Conrad Waako and Syda Namugarura
+
+export default function fibonacci(step) {
+  if (Number.isInteger(step) === false) throw new Error("The input must be an integer");
+  if (step < 0) throw new Error("Steps can not be negative");
 // First test: base case for 0
-  if (n === 0) return 0;       
+  if (step === 0) return 0;       
 // Second test: base case for 1
-  if (n === 1) return 1;       
+  if (step === 1) return 1;       
 
   //set initial variable values
   let prev = 0;
   let curr = 1;
   //loop through values to calculate next value until number n
-  for (let i = 2; i <= n; i++) {
+  for (let i = 2; i <= step; i++) {
     let next = prev + curr;
-    prev = curr;               // Update prev to curr
-    curr = next;               // Update curr to next
+    // Update prev to curr
+    prev = curr;     
+    // Update curr to next
+    curr = next;               
   }
-  return curr;                 // Return the nth Fibonacci number
+  // Return the Fibonacci number for the current step
+  return curr;                 
 }
